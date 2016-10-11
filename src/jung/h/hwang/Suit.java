9400304e917ec30df2c6a6d74f7a0a83cdf5dfe0
junghhwang <jh5@illinois.edu>
2016-10-11 17:13:18 -0500

    added tostring to Suit enum changed the shuffle method in deck got rid of the override of player.equals

--- src/jung/h/hwang/Suit.java
+++ src/jung/h/hwang/Suit.java
@@ -4,5 +4,9 @@ package jung.h.hwang;
  * Created by solmoms on 10/6/2016.
  */
 public enum Suit {
-    CLUBS, HEARTS, DIAMONDS, SPADES, JOKER
+    CLUBS, HEARTS, DIAMONDS, SPADES, JOKER;
+
+    public String toString() {
+        return name().charAt(0) + name().substring(1).toLowerCase();
+    }
 }
