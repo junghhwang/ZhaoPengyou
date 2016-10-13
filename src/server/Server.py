4e9f277a9ab11e43ee5bfea6b2459e2e36c69557
Dingwei Deng <Devin.deng.azn@gmail.com>
2016-10-12 21:33:10 -0500

    This is the server

--- src/server/Server.py
+++ src/server/Server.py
@@ -1,1 +1,12 @@
 print "hello world"
+import socket
+soc = socket.socket()
+host = "localhost"
+port = 4444
+soc.bind((host,port))
+soc.listen(5)
+while True:
+    conn,addr=soc.accept()
+    print("Got connection from", addr)
+    msg=conn.rev(1024)
+    print(msg)
